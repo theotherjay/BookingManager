6a1b709b3d27b1864ab3ecb3c28e5f2ad8c31eda
theotherjay <theotherjay@users.noreply.github.com>
2020-12-08 08:56:59 -0500

    added more stuff

--- BookingMgr/Program.cs
+++ BookingMgr/Program.cs
@@ -7,6 +7,8 @@ namespace BookingMgr
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("more shit added");
         }
     }
 }
